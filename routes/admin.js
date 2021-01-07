a1dc1ef0be5bc7ed028fe6dfdb4ddcc52ec2ce1d
Grigorii Dvoeglazov <dvoegor@gmail.com>
2021-01-07 23:20:31 +0400

    added exporting DB

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const pool = require('../pools/pool');
+// const http = require('http')
+const exportFromJSON = require('export-from-json')
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -38,6 +40,162 @@ router.get('/', (req, res) => {
         req.session.success = false;
         res.status(403).redirect("/login")
     }
+})
+
+router.get('/download/txt', function (req, res, next) {
+    pool.query(`SELECT * FROM crossWords`, function (err, resultData) {
+        if (err) {
+            res.send(err);
+        } else {
+            const data = resultData;
+            const fileName = 'crossWords'
+            const exportType = 'txt'
+
+            const result = exportFromJSON({
+                data,
+                fileName,
+                exportType,
+                processor (content, type, fileName) {
+                    switch (type) {
+                        case 'txt':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'json':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'csv':
+                            res.setHeader('Content-Type', 'text/csv')
+                            break
+                        case 'xls':
+                            res.setHeader('Content-Type', 'application/vnd.ms-excel')
+                            break
+                    }
+                    res.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                    return content
+                }
+            })
+
+            res.write(result)
+            res.end()
+        }
+    });
+})
+
+router.get('/download/json', function (req, res, next) {
+    pool.query(`SELECT * FROM crossWords`, function (err, resultData) {
+        if (err) {
+            res.send(err);
+        } else {
+            const data = resultData;
+            const fileName = 'crossWords'
+            const exportType = 'json'
+
+            const result = exportFromJSON({
+                data,
+                fileName,
+                exportType,
+                processor (content, type, fileName) {
+                    switch (type) {
+                        case 'txt':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'json':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'csv':
+                            res.setHeader('Content-Type', 'text/csv')
+                            break
+                        case 'xls':
+                            res.setHeader('Content-Type', 'application/vnd.ms-excel')
+                            break
+                    }
+                    res.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                    return content
+                }
+            })
+
+            res.write(result)
+            res.end()
+        }
+    });
+})
+
+router.get('/download/csv', function (req, res, next) {
+    pool.query(`SELECT * FROM crossWords`, function (err, resultData) {
+        if (err) {
+            res.send(err);
+        } else {
+            const data = resultData;
+            const fileName = 'crossWords'
+            const exportType = 'csv'
+
+            const result = exportFromJSON({
+                data,
+                fileName,
+                exportType,
+                processor (content, type, fileName) {
+                    switch (type) {
+                        case 'txt':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'json':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'csv':
+                            res.setHeader('Content-Type', 'text/csv')
+                            break
+                        case 'xls':
+                            res.setHeader('Content-Type', 'application/vnd.ms-excel')
+                            break
+                    }
+                    res.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                    return content
+                }
+            })
+
+            res.write(result)
+            res.end()
+        }
+    });
+})
+
+router.get('/download/xls', function (req, res, next) {
+    pool.query(`SELECT * FROM crossWords`, function (err, resultData) {
+        if (err) {
+            res.send(err);
+        } else {
+            const data = resultData;
+            const fileName = 'crossWords'
+            const exportType = 'xls'
+
+            const result = exportFromJSON({
+                data,
+                fileName,
+                exportType,
+                processor (content, type, fileName) {
+                    switch (type) {
+                        case 'txt':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'json':
+                            res.setHeader('Content-Type', 'text/plain')
+                            break
+                        case 'csv':
+                            res.setHeader('Content-Type', 'text/csv')
+                            break
+                        case 'xls':
+                            res.setHeader('Content-Type', 'application/vnd.ms-excel')
+                            break
+                    }
+                    res.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+                    return content
+                }
+            })
+
+            res.write(result)
+            res.end()
+        }
+    });
 })
 
 module.exports = router;
